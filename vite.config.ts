1b663a3bd336c6f54372f0834e52c3dce811e847
ksekine <ksekine.itssp2064@gmail.com>
2026-03-05 17:05:15 +0900

    First commit.

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: new URL("index.html", import.meta.url).pathname,
+        privacyJa: new URL("privacy/ja/index.html", import.meta.url).pathname,
+        privacyEn: new URL("privacy/en/index.html", import.meta.url).pathname,
+        termsJa: new URL("terms/ja/index.html", import.meta.url).pathname,
+        termsEn: new URL("terms/en/index.html", import.meta.url).pathname,
+      },
+    },
+  },
+});
